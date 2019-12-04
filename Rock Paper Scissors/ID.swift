92d0b7e77bc256eb8da887fa9bcbdd1b428532af
Aron <ockersar@mail.gvsu.edu>
2019-12-03 19:05:51 -0500

    Added RPS.html

---
+++ Rock Paper Scissors/ID.swift
@@ -0,0 +1,13 @@
+//
+//  Reference.swift
+//  Rock Paper Scissors
+//
+//  Created by Carson Uecker-Herman on 11/26/19.
+//  Copyright © 2019 armin. All rights reserved.
+//
+
+import Foundation
+class ID {
+    static let LEADERBOARD_ID = "ueckerherman.ockerse.rps.leaderboard"
+    static let WIN_ID = "ueckerherman.ockerse.rps.WinOneGame"
+}
